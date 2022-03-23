414ee3843fdcdde4362face1b4f804c245dca978
gokhan.guzelyurt <Gok26han@>
2022-03-23 02:04:54 +0100

    Initial commit

---
+++ src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
@@ -0,0 +1,32 @@
+package com.cydeo.employeeregistrationproject.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    @NotNull      //"" >except null,anything can be accepted
+    @NotEmpty     //" " >except null and empty String anything can be accepted
+    @NotBlank     //" " "" null > except null,empty String and only space anything can be accepted
+    private String firstName;
+    private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+}
